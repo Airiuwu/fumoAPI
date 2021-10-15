1563efbf1528ca14bbf488ecc57ff9262689e02c
Jacob <61052022+Airiuwu@users.noreply.github.com>
2021-10-15 00:32:38 -0400

    add non async version

--- fumoAPI/fumoAPI.py
+++ fumoAPI/fumoAPI.py
@@ -1,8 +1,15 @@
-import aiohttp, json
+import aiohttp, json, requests
+
+url = 'https://fumos.live/api/fumo'
 
 async def pickRandomFumo():
 	async with aiohttp.ClientSession() as session:
-		async with session.get("https://fumos.live/api/fumo") as requestFumo:
+		async with session.get(url) as requestFumo:
 			fumoAPIText = await requestFumo.text()
 			fumoJson = json.loads(fumoAPIText)
 			return fumoJson["fumo"]
+
+def randomFumo():
+	requestFumo = requests.get(url).text
+	fumoJson = json.loads(requestFumo)
+	return fumoJson["fumo"] 
